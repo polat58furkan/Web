159d4938ed2105609bb4d29a41f05dcf46fda083
polat58furkan <polat58furkan@hotmail.com>
2024-11-30 14:50:53 +0300

    new file:   data.json 	new file:   index.html 	new file:   script.js 	new file:   static/images/Anasayfa/Anasayfa.png 	new file:   static/images/a.jpg 	new file:   static/images/b.jpg 	new file:   static/images/bg.jpg 	new file:   static/images/c.jpg 	new file:   static/images/d.jpg 	new file:   static/images/product1.jpg 	new file:   static/images/product2.jpg 	new file:   "static/images/\303\274r\303\274n1/foto.jpg" 	new file:   "static/images/\303\274r\303\274n1/product1.jpg" 	new file:   static/styles3.css 	new file:   styles.css

---
+++ script.js
@@ -0,0 +1,18 @@
+// JSON dosyasını oku
+fetch('data.json')
+    .then(response => response.json())
+    .then(items => {
+        const product = document.getElementById('product');
+        for (let item of items) {
+            product.innerHTML += `
+                <div class="product" >
+                    <div>
+                        <h3><strong>${item.name}</strong></h3>
+                        <img src="${item.image}" alt="${item.name}" style="max-width:200px;">
+                    </div>    
+                    <p><strong>${item.price} ₺ </strong></p>
+                    <p>${item.description}</p>
+                </div>
+            `;
+        }
+    });
